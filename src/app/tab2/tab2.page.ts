a0e338a5afc6bded96ecfc910609eae60a63d6ea
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-11 22:49:56 +0200

    26. Importo el ngForOf en el controlador de Tab2Page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -11,13 +11,14 @@ import {
 } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { PhotoService } from '../services/photo.service';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, NgForOf]
 })
 export class Tab2Page implements OnInit {
 
