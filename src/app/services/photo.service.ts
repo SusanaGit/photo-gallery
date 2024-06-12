794fa95a6dc5fc1f0b74cd762f68eb27df3515de
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-12 16:43:12 +0200

    28. Importamos Ionic Platform API en el servicio PhotoService

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Platform } from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
