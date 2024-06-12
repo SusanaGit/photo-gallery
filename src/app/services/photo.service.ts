5a037637cd170f97a4942c0b53349a39ef91f3b0
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-12 21:17:52 +0200

    43. Añado el método deletePicture(photo: UserPhoto, position: number) al servicio PhotoService para que se elimine la foto seleccionada del array de fotos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -65,6 +65,27 @@ export class PhotoService {
     };
   }
 
+  // delete photo
+  public async deletePicture(photo: UserPhoto, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+
+    // Update photos array cache by overwriting the existing photo array
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+
+    // delete photo file from filesystem
+    const filename = photo.filepath
+      .substr(photo.filepath.lastIndexOf('/') + 1);
+
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data
+    });
+  }
+
   public async loadSaved() {
     // Retrieve cached photo array data
     const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
